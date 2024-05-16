4f064613d237240699635fc42fc3d340de3668cc
Josue Santamaria <josuexsanta@gmail.com>
2024-05-16 08:43:13 -0600

    add chamnges

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  return <h1>Hello, world Josue test!</h1>;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
